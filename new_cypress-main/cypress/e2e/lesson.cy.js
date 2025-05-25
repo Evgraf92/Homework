b945dca3a5cd1bdbd65a6ac88986a8672307a4ce
Evgraf92 <ermolenkoep@yandex.ru>
2025-05-25 21:01:07 +0300

    Add files via upload

--- new_cypress-main/cypress/e2e/lesson.cy.js
+++ new_cypress-main/cypress/e2e/lesson.cy.js
@@ -41,6 +41,27 @@ describe('Проверка авторизации', function () {
         cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
     })
+     it('Неверный логин и верный пароль', function () {
+        cy.visit('https://login.qa.studio');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#mail').type('notgerman@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    });
+
+    it('Проверка приведения логина к строчным буквам (ожидаемый баг)', function () {
+        cy.visit('https://login.qa.studio');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Авторизация прошла успешно'); // Этот тест должен упасть
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    });
 })
 
 
